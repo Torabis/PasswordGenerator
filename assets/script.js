75fe712a3d808a61da0420f6bf5b58773537cca6
Torabis <sadihome@gmail.com>
2020-08-09 11:19:33 -0700

    assignment codes

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,19 @@
 // Assignment code here
-
+const results = document.querySelector("#password");
+const UNInum =[48,57];
+const UNIupper = [65,90];
+const UNIlower = [97,122];
+const UNIsym = [33,47];
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+document.querySelector("#generate").addEventListener('click', ()=> {
+  const length = document.querySelector("#length").value;
+  const upper = document.querySelector("#upper").checked;
+  const lower = document.querySelector("#lower").checked;
+  const numbers = document.querySelector("#numeric").checked;
+  const symbols = document.querySelector("#special").checked;
+  
+  const randSelector = [];
+  const password = [];
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
